a94d4f5c6919e8dabb3e39052add77bcbda70a97
Ronald <ronaldreyjr061201@gmail.com>
2023-03-07 23:47:01 +0800

    Added function to remove the last number input

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-//main
-let display = document.querySelector('.screen');
-let buttons = document.querySelectorAll('button');
-let a = 0;
-let b = 0;
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        if(button.value == '-' || button.value == '+' || button.value == '*' || button.value == '/' ){
-            // display.textContent = "";
-            // console.log(button.value);
-            switch(button.value){
-                case '+': a = Number(display);
-                          
-            }
-        }else if(button.value == '0' || button.value == '1' || button.value == '2' || button.value == '3' || button.value == '4' || button.value == '5' || button.value == '6' || button.value == '7' || button.value == '8' || button.value == '9'){
-            console.log(button.value);
-        }else if(button.value == 'Clear'|| button.value =='Reset'){
-            switch(button.value){
-                case 'Reset' : display.innerHTML = "";
-                case 'Clear' : removeLastIndex(display.textContent);
-            }
-        }
-    })
-})
 //functions
-function removeLastIndex(node){
-    return node.textContent.splice(0,node.textContent.length-1);
+function removeLastIndex(display){
+    let string = display.textContent;
+    return string.slice(0,string.length - 1);
 }
 function add(a,b){
     return a+b;
@@ -54,3 +31,25 @@ function operate(operator,a,b){
         default: ;
     }
 }
+//main
+let display = document.querySelector('.screen');
+let buttons = document.querySelectorAll('button');
+let a = 0;
+let b = 0;
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if(button.value == '-' || button.value == '+' || button.value == '*' || button.value == '/' ){
+            switch(button.value){
+                case '+': a = Number(display);
+                          
+            }
+        }else if(button.value == '0' || button.value == '1' || button.value == '2' || button.value == '3' || button.value == '4' || button.value == '5' || button.value == '6' || button.value == '7' || button.value == '8' || button.value == '9'){
+            display.textContent += button.value;
+        }else if(button.value == 'Clear'|| button.value =='Reset'){
+            switch(button.value){
+                case 'Reset' : display.innerHTML = "";
+                case 'Clear' : display.textContent = removeLastIndex(display);
+            }
+        }
+    })
+})
