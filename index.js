5592595276daafdb45d6c2acfebdabd18b0ec8a6
Ronald <ronaldreyjr061201@gmail.com>
2023-03-08 10:26:02 +0800

    changed parseInt to parseFloat

--- index.js
+++ index.js
@@ -18,17 +18,29 @@ function divide(a,b){
     }
     return "ERROR";
 }
-function operate(operator,a,b){
-    switch(operator){
-        case 'add': add(a,b);
+function operate(button,a,b){
+    switch(button.value){
+        case '+': 
+                    a = Number.parseFloat(display.textContent);
+                    display.textContent = '';
+                    display.textContent = add(a,b);
                     break;
-        case 'subtract': subtract(a,b);
-                         break;
-        case 'multiply': multiply(a,b);
-                         break;
-        case 'divide': divide(a,b);
-                       break;
-        default: ;
+        case '-': 
+                    a = Number.parseFloat(display.textContent);
+                    display.textContent = '';
+                    display.textContent = subtract(a,b);
+                    break;
+        case '*':   b = 1;
+                    a = Number.parseFloat(display.textContent);
+                    display.textContent = '';
+                    display.textContent = multiply(a,b);
+                    a = Number.parseFloat(display.textContent);
+                    break;
+        case '/':   b = 1;
+                    display.textContent = divide(a,b);
+                    a = Number.parseFloat(display.textContent);
+                    break;
+        default: display.textContent = '';
     }
 }
 //main
@@ -36,21 +48,27 @@ let display = document.querySelector('.screen');
 let buttons = document.querySelectorAll('button');
 let a = 0;
 let b = 0;
+let answer = 0;
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         if(button.value == '-' || button.value == '+' || button.value == '*' || button.value == '/' ){
-            switch(button.value){
-                case '+': display.textContent = add(a,b);
-                          break;
-                case '-':      
-            }
+            operate(button,a,b);
         }else if(button.value == '0' || button.value == '1' || button.value == '2' || button.value == '3' || button.value == '4' || button.value == '5' || button.value == '6' || button.value == '7' || button.value == '8' || button.value == '9'){
             display.textContent += button.value;
         }else if(button.value == 'Clear'|| button.value =='Reset'){
             switch(button.value){
                 case 'Reset' : display.innerHTML = "";
+                                a=0;
+                                b=0;
+                                break;
                 case 'Clear' : display.textContent = removeLastIndex(display);
+                                break;
             }
+        }else if(button.value == '(-)'){
+                display.textContent = Number.parseFloat(display.textContent) * -1;
+        }else if(button.value == '.'){
+            display.textContent += '.';
+            button.setAttribute('disabled','');
         }
     })
-})
+});
